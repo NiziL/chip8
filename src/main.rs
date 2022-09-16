af68d13233ebfe885e745e6fb4641e1d48ccf16a
Théo BL <theo.biasutto-lervat@inria.fr>
2022-09-16 23:43:21 +0200

    :hammer: changing keypad position (only works on a qwerty)

--- src/main.rs
+++ src/main.rs
@@ -38,27 +38,27 @@ fn main() {
             .unwrap();
 
         // update key press state
-        // minifb cannot detect &é" from my azerty keyboard
-        // alternative solution : AZER QSDF UIOP JKLM
+        // minifb cannot detect KeyX from an azerty keyboard
+        // alternative solution : AZER QSDF UIOP JKLM ?
         chip8.reset_keypad();
         for key in window.get_keys() {
             match key {
-                minifb::Key::A => chip8.press_key(0x1),
-                minifb::Key::Z => chip8.press_key(0x2),
-                minifb::Key::E => chip8.press_key(0x3),
-                minifb::Key::R => chip8.press_key(0xC),
+                minifb::Key::Key1 => chip8.press_key(0x1),
+                minifb::Key::Key2 => chip8.press_key(0x2),
+                minifb::Key::Key3 => chip8.press_key(0x3),
+                minifb::Key::Key4 => chip8.press_key(0xC),
                 minifb::Key::Q => chip8.press_key(0x4),
-                minifb::Key::S => chip8.press_key(0x5),
-                minifb::Key::D => chip8.press_key(0x6),
-                minifb::Key::F => chip8.press_key(0xD),
-                minifb::Key::U => chip8.press_key(0x7),
-                minifb::Key::I => chip8.press_key(0x8),
-                minifb::Key::O => chip8.press_key(0x9),
-                minifb::Key::P => chip8.press_key(0xE),
-                minifb::Key::J => chip8.press_key(0xA),
-                minifb::Key::K => chip8.press_key(0x0),
-                minifb::Key::L => chip8.press_key(0xB),
-                minifb::Key::M => chip8.press_key(0xF),
+                minifb::Key::W => chip8.press_key(0x5),
+                minifb::Key::E => chip8.press_key(0x6),
+                minifb::Key::R => chip8.press_key(0xD),
+                minifb::Key::A => chip8.press_key(0x7),
+                minifb::Key::S => chip8.press_key(0x8),
+                minifb::Key::D => chip8.press_key(0x9),
+                minifb::Key::F => chip8.press_key(0xE),
+                minifb::Key::Z => chip8.press_key(0xA),
+                minifb::Key::X => chip8.press_key(0x0),
+                minifb::Key::C => chip8.press_key(0xB),
+                minifb::Key::V => chip8.press_key(0xF),
                 _ => {}
             }
         }
